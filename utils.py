125fde9485716f3582e89ce52193010f9217d044
ferrum76 <daniil.zheleznyakov.2000@gmail.com>
2024-02-24 14:40:05 +0300

    Add filter data

--- utils.py
+++ utils.py
@@ -3,6 +3,11 @@ import io
 
 
 def get_data_from_json(filename: str) -> list[dict]:
+    """
+    Данная функция считывает json файл и преобразует его в список словарей
+    :param filename: путь до файла
+    :return: список словарей
+    """
     if type(filename) is not str:
         return []
 
@@ -11,3 +16,19 @@ def get_data_from_json(filename: str) -> list[dict]:
     file.close()
 
     return data
+
+
+def filter_data(operations_data: list[dict]) -> list[dict]:
+    """
+    Данная функция отфильтровывает основной массив и возвращает массив из выполненных операций
+    :param operations_data: массив оъектов операций
+    :return: отфильтрованный массив по выполненным операциям
+    """
+    if len(operations_data) == 0:
+        return []
+    res = []
+    for data in operations_data:
+        if data.get('state') == 'EXECUTED':
+            res.append(data)
+
+    return res
